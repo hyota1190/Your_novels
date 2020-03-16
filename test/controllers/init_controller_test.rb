401a0c1c6d50534f0cb4545c3c1b8924fe8043dd
github user name <iron.hulu@gmail.com>
2020-03-16 16:28:30 +0900

    Complete Initial edit
    Home、Help、Aboutページ作成の完了（未編集）

--- test/controllers/init_controller_test.rb
+++ test/controllers/init_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
 class InitControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "YourNovels"
+  end
+
   test "should get home" do
     get root_path
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get init_help_path
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get init_about_path
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
